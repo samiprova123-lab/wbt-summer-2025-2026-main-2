2c216a30f68943cd5b0ed4badce2b69ccca9b86f
Fahim Shahariar <samiprova123@gmail.com>
2026-08-16 09:46:24 +0600

    Create db.php for database connection
    Added database connection settings and connection logic.

---
+++ wbt/db.php
@@ -0,0 +1,15 @@
+<?php
+/* ---------- Database connection settings ---------- */
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "portfolio_db";
+
+
+/* ---------- Connect (MySQLi, procedural) ---------- */
+$conn = mysqli_connect($dbHost, $dpUser, $dpPass, $dbName);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
